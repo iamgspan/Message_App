38164986385b9cd8fbfbab02dee55ea1da6c3951
Spandana Gannavarapu <iamgspan@gmail.com>
2020-10-05 21:54:06 +0530

    3rd commit - Working changes for message display

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import Message from './components/message-component/Message';
-import { Button, FormControl, Input, InputLabel } from '@material-ui/core';
+import Message from './components/message-component/Message'; //import Message component to be displayed
+import { FormControl, IconButton, Input } from '@material-ui/core';
 import SendIcon from '@material-ui/icons/Send';
-import firebaseDB from './firebaseConfig';
+import firebaseDB from './firebaseConfig';  //import firebase database
 import firebase from 'firebase';
+import FlipMove from 'react-flip-move';
 
 function App() {
 
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState('');  
   const [messages, setMessages] = useState([]); 
-  const [ username, setUsername ] = useState('');             //State to remember the user
+  const [ username, setUsername ] = useState('');
   
   //We can have multiple useEffect for different purposes.
   //Here we are listening for changes in our firestore database for any changes on add/remove messages.
@@ -21,56 +22,56 @@ function App() {
   //realtime database listener
   //We are ordering by the timestamp in ascending order.
   useEffect(() => {
-    firebaseDB.collection('Messages').orderBy('timestamp', 'asc').onSnapshot(snapshot => {
-      setMessages(snapshot.docs.map(doc => doc.data()))
+    firebaseDB.collection('Messages').orderBy("timestamp", "asc").onSnapshot(snapshot => {
+      setMessages(snapshot.docs.map(doc => doc.data() ));
     })
   }, [])
-
+  
   useEffect(() => {
-    //const username = prompt('Enter your name...');          //----this name when enetered by user gets stored in the username variable. 
-    setUsername(prompt('Please enter your name...'));         // So instead of creating new variable, we can set the user input in setUsername    
-  }, [])
-
+    //const username = prompt('Enter your name...');          //----this name when enetered by user gets stored in the username variable.
+    setUsername(prompt("Please enter your name...")); // So instead of creating new variable, we can set the user input in setUsername
+  }, []);
 
   const sendMessage = (e) => {
-    e.preventDefault();                                       //prevents the page to refresh automatically everytime on send button hit
-
-    //The below code stores the messages being sent in database
+    e.preventDefault();
     //We are adding the timestamp so that we can display the messages in the order by timestamp
-    firebaseDB.collection('Messages').add({
+    firebaseDB.collection("Messages").add({
       username: username,
       message: input,
-      timestamp: firebase.firestore.FieldValue.serverTimestamp()
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
     });
-
-    //all the functionality and logic for sending message goes here----------    
-    setMessages([...messages, { username: username, message: input }]);              //we append each input to whatever exists initially
-    setInput('');                                             //this clears the input field after sending the message
+    setMessages([...messages, {username:username, message:input}]); //we append each input to whatever exists initially
+    setInput(''); //this clears the input field after sending the message
   }
   
   return (
     <div className="App">
-      <h1>My ChatBox</h1>
-      {/* We will be needing input field to type the message, a button to send the messsage, and the message itself. */}
-      <form>
-        <FormControl>        
-          <InputLabel htmlFor="my-input">Enter your message...</InputLabel>
-          <Input type="text" value={input} onChange={e => setInput(e.target.value) } />
-          <Button type="submit" disabled={!input} onClick={sendMessage} >
-            <SendIcon />
-          </Button>
-        </FormControl>
-
-        {/* //Here we display the messages on the screen
-            //iterating over messages array and displaying each message
-            //this username we are passing as props, and we are mapping to the name property in message object
-            //displaying the whole message as other people might send it. username Here is the one when we login */}        
-        {
-          messages.map(message => (            
-            <Message username={username} message={message} />                                
-          ))          
-        }                  
-      </form>
+      <div className='header-logo'>
+        <h1>Friends and Gossip...</h1>
+        <img src="https://image.shutterstock.com/image-vector/sphere-speech-bubble-logo-vector-600w-363494414.jpg" alt="logo" width="80" height="80"/>
+      </div>      
+      <p>Welcome <strong>{username}</strong> !! Send in your message to continue the chat...</p>
+      <div className='container'>
+        <form className='app__form'> 
+          <FormControl className="app__formControl">
+            <Input className='app__input' type='text' value={input} onChange={ e => setInput(e.target.value)} placeholder="Enter your message..." />
+            <IconButton className='app__iconBtn' disabled={!input} type='submit' onClick={sendMessage}>
+              <SendIcon />
+            </IconButton>
+          </FormControl>        
+        </form>
+        {/* //Here we display the messages on the screen, wrapped inside FlipMove for animation effect.
+        //iterating over messages array and displaying each message
+        //this username we are passing as props, and we are mapping to the name property in message object
+        //displaying the whole message as other people might send it. username Here is the one when we login */}
+        <FlipMove className='container'>
+          {
+            messages.map(message => (
+              <Message username={username} message={message} />
+            ))
+          }
+        </FlipMove>
+      </div>
     </div>
   );
 }
--- src/components/message-component/Message.jsx
+++ src/components/message-component/Message.jsx
@@ -1,19 +1,28 @@
-import React from 'react';
 import { Card, CardContent, Typography } from '@material-ui/core';
+import React, {forwardRef} from 'react'
 import './Message.css';
 
-function Message({message, username}) {
-
+//Here we use forwardRef to track what's changing/moving/flipping.
+//We need to change the functional component into ES6 format and wrap in inside forwardRef().
+//So we basically forwardRef is a higher order function wrapping the functionalcomponent within it.
+//Here we get a ref which provides a reference to an object.
+//We need to attach this ref to the div which we want to keep track of, so it knows what elements are changing/moving.
+//This is an example of higher order component
+const Message = forwardRef( ({username, message}, ref) => {
     const isUser = username === message.username;
+
+
     return (
-        <div className={`message__card ${isUser && 'message__user'}`}>
-            <Card className={isUser ? 'message__selfLoginCard' : 'message__guestUserCard'}>
-                <CardContent>                  
-                    <Typography color="white" variant="p" component="p"> {message.username}: {message.message} </Typography>
-                </CardContent>                
-            </Card>           
+        <div ref={ref} className={`message__card ${isUser && "message__user"}`}>
+            <Card className={isUser ? "message__selfLoginCard" : "message__guestUserCard"}>
+                <CardContent>
+                    <Typography variant='p' component='p' >
+                    <strong>{!isUser && `${message.username || "Unknown User"}: `}</strong>{message.message}
+                    </Typography>
+                </CardContent>
+            </Card>            
         </div>
     )
-}
+})
 
-export default Message;
+export default Message
